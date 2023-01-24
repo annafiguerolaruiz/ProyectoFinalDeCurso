da9a2c53339990b889b15142ad1db0dbf94fcf56
annafiguerolaruiz <91455277+annafiguerolaruiz@users.noreply.github.com>
2023-01-24 10:42:10 +0100

    nre

--- Assets/Scripts/MainMneu.cs
+++ Assets/Scripts/MainMneu.cs
@@ -5,8 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMneu : MonoBehaviour
 {
- public void SceneLevel1()
+    public void SceneLevel1()
     {
         SceneManager.LoadScene("Level_1");
+    }
+
+    public void  SceneLevel2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+    public void SceneLevel3()
+    {
+        SceneManager.LoadScene("Level_3");
+    }
+
+    public void SceneLevel4()
+    {
+        SceneManager.LoadScene("Level_4");
+    }
+
+    public void SceneLevel5()
+    {
+        SceneManager.LoadScene("Level_5");
     }
 }
