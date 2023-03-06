48fb76db4f84e079817b583adef3a86aeb5d1104
annafiguerolaruiz <91455277+annafiguerolaruiz@users.noreply.github.com>
2023-03-06 13:56:50 +0100

    as

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,11 +9,34 @@ public class UIManager : MonoBehaviour
     //Record texts on screen:
     public TextMeshProUGUI CurrentScoreText;
     public TextMeshProUGUI BestScoreText;
-  
+
+    public Slider slider;
+    public Text ActualLevel;
+    public Text NextLevel;
+
+    public Transform topTransform;
+    public Transform goalTransform;
+
+    public Transform ball;
+
 
     void Update()
     {
         CurrentScoreText.text = "Score:" + GameManager.GM.CurrentScore;
         BestScoreText.text = "Best:" + GameManager.GM.BestScore;
+
+        ChangeSliderLevelAnProgress();
     }
+
+
+    public void ChangeSliderLevelAnProgress()
+    {
+     
+
+        float totalDistance = (topTransform.position.y = goalTransform.position.y);
+        float distanceLeft = totalDistance = (ball.transform.position.y - goalTransform.position.y);
+        float value = (distanceLeft / totalDistance);
+        slider.value = Mathf.Lerp(slider.value,value,5);
+    }
+
 }
