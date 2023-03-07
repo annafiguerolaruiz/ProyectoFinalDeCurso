04882ec8b89838d60f8ccd5ba1f77d54ae26b312
annafiguerolaruiz <91455277+annafiguerolaruiz@users.noreply.github.com>
2023-03-07 13:20:13 +0100

    AS

--- Assets/Scripts/MainMneu.cs
+++ Assets/Scripts/MainMneu.cs
@@ -5,10 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMneu : MonoBehaviour
 {
+
+    public GameObject Settings;
     //Change scenes
     public void SceneLevel(int value)
     {
         GameManager.GM.CurrentLevel = value;
         SceneManager.LoadScene(GameManager.GM.CurrentLevel);
     }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+    public void Niveles()
+    {
+        SceneManager.LoadScene("Levels");
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void Start()
+    {
+        Settings.SetActive(false);
+    }
+    public void SettingsButton()
+    {
+        if (Settings.activeInHierarchy == false) { Settings.SetActive(true); } else { Settings.SetActive(false); }
+        
+    }
+
+
 }
